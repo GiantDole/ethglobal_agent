75a48e057ecdea5c7efa3ef4d435a369882ad0be
GiantDole <aak2229@columbia.edu>
2025-02-05 16:17:29 +0100

    Adding example frontend passing cookie for Shouvik <3

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,8 +7,21 @@ import { cookies } from "next/headers";
 export default async function Page() {
   const cookieStore = await cookies()
   const cookieAuthToken = cookieStore.get("privy-token")?.value;
+  const cookieIdToken = cookieStore.get("privy-id-token")?.value;
 
   if (cookieAuthToken){
+    //query localhost:3000/api/register
+    const response = await fetch("http://localhost:8000/api/user/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        // Manually set the Cookie header with the tokens
+        "Cookie": `privy-token=${cookieAuthToken}; privy-id-token=${cookieIdToken}`
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+
     const PRIVY_APP_ID = process.env.NEXT_PUBLIC_PRIVY_APP_ID;
     const PRIVY_APP_SECRET = process.env.PRIVY_APP_SECRET;
     const client = new PrivyClient(PRIVY_APP_ID!, PRIVY_APP_SECRET!);
